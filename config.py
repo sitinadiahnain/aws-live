a912b53f507589ae6dad9ba6faf12965e2505f99
sitinadiahnain <sitinadiahnain@gmail.com>
2025-12-10 15:16:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chjzgt6cecf8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bmit3273"
+customdb = "employee"
+custombucket = "sitinadiah-employee"
+customregion = "us-east-1"
+
